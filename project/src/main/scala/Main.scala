1aef63cebb61b9c6bc4f5617c645ea08b86e36be
Etienne Chartrand <chartrand.etienne@gmail.com>
2013-08-20 13:27:31 -0400

    radical

--- project/src/main/scala/Main.scala
+++ project/src/main/scala/Main.scala
@@ -4,42 +4,11 @@ import scala.io.Source
 
 object Main extends App {
   println("hello, world")
-  var line2 = "x + 2"
   try {
     for(line <- Source.fromFile("expr").getLines()) {
-      println("\n\n\n\tsource :\t" + line)  
-      println("\tsource2:\t" + line2)  
-      var exprTree = new Expression(p.apply(line).toTree)
-      println("\tpreflat:\t" + exprTree)
-      val exprFlattened = exprTree.toFlatten
-      println("\tpresimp:\t" + exprFlattened)
-      var powerSerie = exprFlattened.simplify
-      var exprTree2 = new Expression(p.apply(line2).toTree)
-      var powerSerie2 = exprTree2.toFlatten.simplify
-//      println("\tdeg:\t" + powerSerie.degPowerSeries)
-      val vector1 = powerSerie.toVectorRep
-      val vector2 = powerSerie2.toVectorRep
-      println("\tvec:\t\t" + vector1)
-//      println("\tvev2:\t\t" + vector2)
-//      println("\tsum:\t\t" + (vector1 + vector2).toString)
-//      println("\t\t" + vector1.toString + " // " + vector2.toString)
-//      val div1 = vector1 / vector2
-//      println("\tdiv:\t\t" + div1.toString)
-//      val gcd1 = vector1 gcd vector2
-//      println("\tgcd:\t\t" + gcd1.toString)
-      val deriv1 = vector1.deriv
-      println("\tderiv:\t\t" + deriv1.toString)
-      val gcd1 = vector1 gcd deriv1
-      println("\tgcd:\t\t" + gcd1.toString)
-      
-  
-/*
-      val mult = vector1 * vector2
-      println("\tmult:\t\t" + mult.toString)
-      println("\tmult:\t\t" + (mult / vector2).toString)
-*/
-      line2 = line
-    }
+      val expr = p.apply(line)
+      println("\texpr:\t"+expr.toString+"\n")
+    }  
   } catch {
     case e => println("what the flying fuck: " + e.getMessage)
   }
--- project/src/main/scala/MathNode.scala
+++ project/src/main/scala/MathNode.scala
@@ -4,11 +4,6 @@ import math.{pow, sqrt, cbrt}
 abstract class MathNode {
   def eval(): Double
   def eval(valeurVariables: Map[String, Double]): Double
-//  def toString2: String
-//  def toString3: String
-  def toQueue(level: Int, shift: Int): List[(MathNode, Int, Int)]
-  def toTree: Tree[MathNode]
-//  def toFlatten(x: MathNode): Boolean
   def toFlatten(x: MathNode): (Boolean, (Boolean, Boolean))
 }
 
@@ -18,8 +13,6 @@ case object NullNode extends MathNode {
   def eval() = 0.0
   def eval(valeurVariables: Map[String, Double]) = 0.0
   override def toString = ""
-  def toQueue(level: Int, shift: Int) = Nil
-  def toTree = Nill
   def toFlatten(x: MathNode) = (false, (false, false))
 }
 
@@ -27,8 +20,6 @@ case object NullNode extends MathNode {
 case class VariableNode(value: String) extends MathNode {
   def eval(): Double = eval(Map())
   override def toString = value.toString
-  def toQueue(level: Int, shift: Int) = (this, level, shift) :: Nil
-  def toTree = new Node(this)
   def eval(valeurVariables: Map[String, Double]) = 
     try {
       valeurVariables(value)
@@ -41,8 +32,6 @@ case class VariableNode(value: String) extends MathNode {
 
 case class NumeralNode(value: Int) extends MathNode {
   override def toString = value.toString
-  def toQueue(level: Int, shift: Int) = (this, level, shift) :: Nil
-  def toTree = new Node(this)
   def eval(): Double = eval(Map())
   def eval(valeurVariables: Map[String, Double]) = value.toDouble
   def +(that: NumeralNode) = NumeralNode(that.value + this.value)
@@ -53,10 +42,10 @@ case class NumeralNode(value: Int) extends MathNode {
 
 
 case class BinOpNode(left: MathNode, right: MathNode, op: String) extends MathNode {
-  override def toString = op
+//  override def toString = op
 //  def toString2 = 
 //    "bop(" + op + " " + left.toString2 + " " + right.toString2 + ")"
-//  def toString3 = "(  " + op + "  )"
+  override def toString = "(" + left.toString + " " + op + " " + right.toString + ")"
 
   def apply(leftargnode: NumeralNode, rightargnode: NumeralNode): NumeralNode = {
     val (NumeralNode(leftarg), NumeralNode(rightarg)) = (leftargnode, rightargnode)
@@ -75,9 +64,6 @@ case class BinOpNode(left: MathNode, right: MathNode, op: String) extends MathNo
   }
 
   def this(value: String) = this(NullNode, NullNode, value)
-
-  def toQueue(level: Int, shift: Int) = (this, level, shift) :: left.toQueue(level + 1, shift - 1) ::: right.toQueue(level + 1, shift + 1)
-  def toTree = Node[MathNode](BinOpNode(null, null, op), left.toTree, right.toTree)
 
   def eval(valeurVariables: Map[String, Double]) = { 
     val leftEval = left.eval(valeurVariables)
@@ -106,9 +92,4 @@ case class BinOpNode(left: MathNode, right: MathNode, op: String) extends MathNo
     case BinOpNode(_, _, lop) if ((op == "*") && (lop == "*")) => (true, (false, false))
     case _ => (false, (false, false))
   }
-//  def toFlatten(x: MathNode) = x match {
-////    case BinOpNode(_, _, lop) => ((lop == "*") && (op == "*")) || ((lop == "+") && (op == "+")) || ((lop == "-") && (op == "-"))
-//    case BinOpNode(_, _, lop) => ((op == "+") && (lop == "+"))
-//    case _ => false 
-//  }
 }
